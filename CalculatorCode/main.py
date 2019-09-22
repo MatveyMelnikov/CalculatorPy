bd41ef76900fa42fb2b6b8ce5d55340ce53787ce
MatveyMelnikov <matveyy-melnikov@rambler.ru>
2019-09-22 11:02:15 +0300

    Add calculator code

---
+++ CalculatorCode/main.py
@@ -0,0 +1,50 @@
+import sys
+
+def error_checking(a, b, str):
+    if len(str) != 3:
+        sys.exit()
+    try:
+        a = int(str[0])
+    except ValueError:
+        print("Веденно не число")
+        sys.exit(-1)
+    try:
+        b = int(str[2])
+    except ValueError:
+        print("Веденно не число")
+        sys.exit(-1)
+
+def calculating(a, b, str):
+    c = 0
+    if str[1] == "+":
+        c = a + b
+    elif str[1] == "-":
+        c = a - b
+    elif str[1] == "*":
+        c = a * b
+    elif str[1] == "/":
+        c = a / b
+    elif str[1] == "**":
+        c = a ** b
+    elif str[1] == "//":
+        c = a // b
+    return c
+
+def main():
+    print("My first calulator on python (a +/-/*///**/// b) and enter 'exit' to close app")
+    while 1:
+        str = input()
+        a = 0
+        b = 0
+        if str == "exit":
+            sys.exit(0)
+        str = str.split(" ")
+        error_checking(a, b, str)
+        a = int(str[0])
+        b = int(str[2])
+        c = calculating(a, b, str)
+        print(c)
+
+if __name__ == '__main__':
+    main()
+
